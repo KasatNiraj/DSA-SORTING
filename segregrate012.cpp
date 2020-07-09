aea4784e6b254267766a0e2abc29937065ef393f
KasatNiraj <53268215+KasatNiraj@users.noreply.github.com>
2020-07-09 12:42:31 +0530

    Create segregrate012.cpp

---
+++ segregrate012.cpp
@@ -0,0 +1,21 @@
+void segragate012(int arr[], int n)
+{
+    int low=0,mid=0,high=n-1;
+    while(mid<=high){
+        switch (arr[mid]){
+            case 0:
+                swap(arr[mid],arr[low]);
+                low++;
+                mid++;
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2: 
+                swap(arr[mid],arr[high]);
+                high--;
+                break;
+        }
+    }
+    return;
+}
